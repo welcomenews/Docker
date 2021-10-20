fc20872158326fe5ffbcc3cfda7ae800438cdecb
Игнатов Сергей <welcome-news@mail.ru>
2021-10-20 22:51:10 +0300

    Change screept sh

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+# Скачиваем новые изменения из ветки develop github и пушим их в ветку develop gitlab.
+# Предварительно нужно добавить репозиторий github
+
 cd /home/sergey/Rebrain/devops-engineer-basics/
 git checkout develop
 git pull
